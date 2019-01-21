78f310ee949e96387378cba8659fafb6bd6e3eee
kyled <dupontke@gmail.com>
2019-01-21 10:00:58 -0500

    updated database to mongodb

--- drapp/settings.py
+++ drapp/settings.py
@@ -98,8 +98,8 @@ WSGI_APPLICATION = 'drapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': 'mymongo',
     }
 }
 
